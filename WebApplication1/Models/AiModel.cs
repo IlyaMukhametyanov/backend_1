cd85c41a17911df3b28d80143204a47fdfe0c1f6
IlyaMukhametyanov <ilyamuhametyanov@gmail.com>
2024-06-11 00:35:33 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/AiModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class AiModel
+{
+    public long AiModelId { get; set; }
+
+    public long? AiModelAuthor { get; set; }
+
+    public string? AiModelName { get; set; }
+
+    public string? AiModelDescription { get; set; }
+
+    public virtual User? AiModelAuthorNavigation { get; set; }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class User
+{
+    public long UserId { get; set; }
+
+    public string? UserLogin { get; set; }
+
+    public string? UserPassword { get; set; }
+
+    public string? UserEmail { get; set; }
+
+    public virtual ICollection<AiModel>? AiModels { get; set; } = new List<AiModel>();
+}
